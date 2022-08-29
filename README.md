123a85ed7eab935cab56da6cd7e80e0927501033
Nick Krul <nicholas.krul@gmail.com>
2022-08-29 16:08:09 +1000

    add aws deploy

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # oauth2-service
 OAuth2 Service
+
+A Java Lambda designed to allow lightweight integration into your existing auth stack.
+
+## How to use
+
+There are a few different ways to use this project.
+The recommended way is to deploy and configure
+
+### Deploy and Configure
+Deploy the .zip file as a lambda, and configure the appropriate configuration options.
+
+## Deployment
+A default deployment is provided in the `aws-deploy` folder
--- src/main/java/com/kncept/oauth2/Handler.java
+++ src/main/java/com/kncept/oauth2/Handler.java
@@ -1,29 +1,62 @@
 package com.kncept.oauth2;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyResponseEvent;
 
-public class Handler implements RequestHandler<Map<String,String>, String> {
+public class Handler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
     @Override
     // APIGatewayV2ProxyResponseEvent
-    public String handleRequest(Map<String, String> input, Context context) {
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
         // LambdaLogger logger = context.getLogger();
 
 
-        System.getenv().forEach((key, value) -> System.out.println("env " + key + outputValue(value)));
-        System.getProperties().forEach((key, value) -> System.out.println("prop " + key + outputValue(value)));
+        StringBuilder sb = new StringBuilder();
+        System.getenv().forEach((key, value) -> sb.append("env " + key + outputValue(value)));
+        System.getProperties().forEach((key, value) -> sb.append("prop " + key + outputValue(value)));
 
-        return "200 OK";
+        sb.append("\n");
+
+        sb.append("logStreamName " + outputValue(context.getLogStreamName()));
+        sb.append("awsRequestId " + outputValue(context.getAwsRequestId()));
+        sb.append("remainingMillisTime " + outputValue(context.getRemainingTimeInMillis()));
+
+        sb.append("rawPath " + outputValue(input.getRawPath()));
+        sb.append("rawQueryString " + outputValue(input.getRawQueryString()));
+
+        sb.append("version " + outputValue(input.getVersion()));
+        try { // if (input.getRequestContext().getHttp() != null) { // can't even CHECK for null without the NPE being thrown
+            sb.append("httpPath " + outputValue(input.getRequestContext().getHttp().getPath()));
+            sb.append("httpMethod " + outputValue(input.getRequestContext().getHttp().getMethod()));
+        } catch (NullPointerException e) {
+            // because amazon thinks it's clever throwing an NPE and not allowing it to be checked :/
+        }
+
+        sb.append("b64Encoded " + outputValue(input.getIsBase64Encoded()));
+
+        sb.append("body " + outputValue(input.getBody()));
+
+        Map<String, String> headers = new TreeMap<>();
+        headers.put("Content-Type", "text/plain");
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+        response.setHeaders(headers);
+        response.setIsBase64Encoded(false);
+        response.setBody(sb.toString());
+        response.setStatusCode(200);
+        return response;
     }
 
     private static String outputValue(Object value) {
-        if (value == null) return " is null";
-        if (value.equals("")) return " is blank";
-        return " = " + value;
+        if (value == null) return " is null" + "\n";
+        if (value.equals("")) return " is blank" + "\n";
+        return " = " + value + "\n";
     }
 
 }
